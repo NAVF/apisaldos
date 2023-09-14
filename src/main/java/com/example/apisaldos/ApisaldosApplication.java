6c544b654293dc2cdddf6d5f7790adcc5c85d2a1
Neil Armstrong Vasquez Faustino <nvasquef@emeal.nttdata.com>
2023-09-14 08:52:44 -0500

    primer despliegue de proyecto

---
+++ src/main/java/com/example/apisaldos/ApisaldosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisaldos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisaldosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisaldosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apisaldos/ApisaldosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apisaldos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisaldosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
